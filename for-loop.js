77b3df82cd5fe53573901e34143619045a6b6997
CoderMahabub <codermahabub@gmail.com>
2021-08-02 10:36:56 +0600

    For Loop Done

---
+++ for-loop.js
@@ -0,0 +1,24 @@
+
+// for (var roastGiven = 0; roastGiven < 10; roastGiven++) {
+//     console.log('Roas den, Please');
+//     console.log(roastGiven);
+// }
+// console.log('Running a for loop');
+// for (var roastGiven = 0; roastGiven < 10; roastGiven++) {
+//     console.log('Roas den, Please');
+// console.log(roastGiven);
+// }
+
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//Find only oddNumber
+// for (var i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+//Find Only Even Number
+for (var i = 0; i <= 30; i += 2) {
+    console.log(i);
+}
