96fc2242e49195e1444c345d39fa77763e5c5afa
CoderMahabub <codermahabub@gmail.com>
2021-08-02 10:07:31 +0600

    While Loop Done

---
+++ while-loop-example.js
@@ -0,0 +1,27 @@
+// Print 0 to 10 
+var number = 0; //Loop variable
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+//Find oddNumber upto 20
+// var oddNumber = 1;
+
+// while (oddNumber < 20) {
+//     console.log(oddNumber);
+//     oddNumber = oddNumber + 2;
+// }
+
+// Find Even Number upto 20
+var evenNumber = 0;
+
+while (evenNumber < 20) {
+    console.log(evenNumber);
+    evenNumber = evenNumber + 2;
+}
