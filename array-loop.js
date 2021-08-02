555d4f8f6e1dcb910e5fd5df82df511581744baf
CoderMahabub <codermahabub@gmail.com>
2021-08-02 11:23:24 +0600

    Array show using for loop

---
+++ array-loop.js
@@ -0,0 +1,18 @@
+// var numbers = [45, 54, 42, 87, 76, 52, 76, 77, 99];
+// for (var i = 0; i < numbers.length; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+
+// var myClassMatesAge = [23, 32, 12, 32, 45, 33, 22, 21, 32];
+// for (i = 0; i < myClassMatesAge.length; i++) {
+//     var ages = myClassMatesAge[i];
+//     console.log(ages);
+// }
+
+var items = ['bottle', 'mobile', 'book', 'fan', 'clothes', 'Kutti'];
+for (i = 0; i <= items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
---
+++ summary.js
@@ -0,0 +1,11 @@
+//Basic While Loop
+var i = 0;
+while (i < 7) {
+    console.log(i);
+    i++;
+}
+
+//Basic For Loop
+for (i = 0; i < 7; i++) {
+    console.log(i);
+}
